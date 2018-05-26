14df1a6190fa495970611072168937691de8953e
Alexander Ewetumo <trinoxf@gmail.com>
2018-05-26 18:15:57 +0100

    Add changes for ArgType

--- ast/ast.go
+++ ast/ast.go
@@ -835,7 +835,6 @@ func (a ArgType) GetStructDeclr() StructDeclaration {
 		Struct:          a.StructObject,
 		Name:            a.Spec.Name.Name,
 		Declr:           a.Pkg,
-		NameWithPackage: fmt.Sprintf("%s.%s", a.Package, a.Spec.Name.Name),
 	}
 }
 
